42bcc754d36d2968fc7e4ca8ed87d2f95adbbbe0
Ritesh Oedayrajsingh Varma <rovarma@gmail.com>
2016-07-07 19:50:54 +0200

    - Updated cinder and openframeworks apps to work with new driver - Both of them now output in BGR instead of RGBA/BGRA - Removed intermediate buffers that are now unnecessary; instead now reads frame data directly to target buffer - Removed yuv -> rgb/bgr conversion code

--- openframeworks/testApp.cpp
+++ openframeworks/testApp.cpp
@@ -1,53 +1,5 @@
 #include "testApp.h"
 
-static const int ITUR_BT_601_CY = 1220542;
-static const int ITUR_BT_601_CUB = 2116026;
-static const int ITUR_BT_601_CUG = -409993;
-static const int ITUR_BT_601_CVG = -852492;
-static const int ITUR_BT_601_CVR = 1673527;
-static const int ITUR_BT_601_SHIFT = 20;
-
-static void yuv422_to_rgba(const uint8_t *yuv_src, const int stride, uint8_t *dst, const int width, const int height)
-{
-    const int bIdx = 2;
-    const int uIdx = 0;
-    const int yIdx = 0;
-    
-    const int uidx = 1 - yIdx + uIdx * 2;
-    const int vidx = (2 + uidx) % 4;
-    int j, i;
-    
-#define _max(a, b) (((a) > (b)) ? (a) : (b))
-#define _saturate(v) static_cast<uint8_t>(static_cast<uint32_t>(v) <= 0xff ? v : v > 0 ? 0xff : 0)
-    
-    for (j = 0; j < height; j++, yuv_src += stride)
-    {
-        uint8_t* row = dst + (width * 4) * j; // 4 channels
-        
-        for (i = 0; i < 2 * width; i += 4, row += 8)
-        {
-            int u = static_cast<int>(yuv_src[i + uidx]) - 128;
-            int v = static_cast<int>(yuv_src[i + vidx]) - 128;
-            
-            int ruv = (1 << (ITUR_BT_601_SHIFT - 1)) + ITUR_BT_601_CVR * v;
-            int guv = (1 << (ITUR_BT_601_SHIFT - 1)) + ITUR_BT_601_CVG * v + ITUR_BT_601_CUG * u;
-            int buv = (1 << (ITUR_BT_601_SHIFT - 1)) + ITUR_BT_601_CUB * u;
-            
-            int y00 = _max(0, static_cast<int>(yuv_src[i + yIdx]) - 16) * ITUR_BT_601_CY;
-            row[2-bIdx] = _saturate((y00 + ruv) >> ITUR_BT_601_SHIFT);
-            row[1]      = _saturate((y00 + guv) >> ITUR_BT_601_SHIFT);
-            row[bIdx]   = _saturate((y00 + buv) >> ITUR_BT_601_SHIFT);
-            row[3]      = (0xff);
-            
-            int y01 = _max(0, static_cast<int>(yuv_src[i + yIdx + 2]) - 16) * ITUR_BT_601_CY;
-            row[6-bIdx] = _saturate((y01 + ruv) >> ITUR_BT_601_SHIFT);
-            row[5]      = _saturate((y01 + guv) >> ITUR_BT_601_SHIFT);
-            row[4+bIdx] = _saturate((y01 + buv) >> ITUR_BT_601_SHIFT);
-            row[7]      = (0xff);
-        }
-    }
-}
-
 //--------------------------------------------------------------
 void testApp::setup(){
     using namespace ps3eye;
@@ -66,8 +18,8 @@ void testApp::setup(){
         bool res = eye->init(640, 480, 60);
         eye->start();
         
-        videoFrame 	= new unsigned char[eye->getWidth()*eye->getHeight()*4];
-        videoTexture.allocate(eye->getWidth(), eye->getHeight(), GL_RGBA);
+        videoFrame 	= new unsigned char[eye->getWidth()*eye->getHeight()*3];
+        videoTexture.allocate(eye->getWidth(), eye->getHeight(), GL_BGR_EXT);
     }
 }
 void testApp::exit(){
@@ -82,10 +34,8 @@ void testApp::update()
 {
     if(eye)
     {
-		uint8_t* new_pixels = eye->getFrame();
-        yuv422_to_rgba(new_pixels, eye->getRowBytes(), videoFrame, eye->getWidth(),eye->getHeight());
-        videoTexture.loadData(videoFrame, eye->getWidth(),eye->getHeight(), GL_RGBA);
-		free(new_pixels);
+		eye->getFrame(videoFrame);
+        videoTexture.loadData(videoFrame, eye->getWidth(),eye->getHeight(), GL_BGR_EXT);
 
         camFrameCount++;
         float timeNow = ofGetElapsedTimeMillis();
