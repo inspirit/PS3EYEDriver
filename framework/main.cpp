572b1213018363c9ca6d27d7221e9065be68a07c
Marcel Smit <marcel303@gmail.com>
2018-07-24 18:19:23 +0200

    Improved Framework test app. - Drawing of the captured audio waveform is now faster. - Combined audio and video test. It works!

--- framework/main.cpp
+++ framework/main.cpp
@@ -17,44 +17,103 @@ static uint8_t imageData[CAM_SX * CAM_SY * 3];
 struct MyAudioCallback : AudioCallback
 {
 	std::vector<float> histories[4];
+	int maxHistoryFrames = 0;
+	int numHistoryFrames = 0;
+	int firstHistoryFrame = 0;
 	
 	MyAudioCallback(const float seconds)
 	{
-		const int numFramesToExpect = seconds * 16000;
-		const int numFramesToReserve = numFramesToExpect * 110 / 100;
+		maxHistoryFrames = seconds * PS3EYEMic::kSampleRate;
 		
 		for (int i = 0; i < 4; ++i)
-			histories[i].reserve(numFramesToReserve);
+		{
+			histories[i].resize(maxHistoryFrames, 0.f);
+		}
 	}
 	
 	virtual void handleAudioData(const AudioFrame * frames, const int numFrames) override
 	{
 		//printf("received %d frames!\n", numFrames);
 		
-		for (int c = 0; c < 4; ++c)
+		for (int i = 0; i < numFrames; ++i)
 		{
-			auto & history = histories[c];
-			
-			for (int i = 0; i < numFrames; ++i)
+			for (int c = 0; c < 4; ++c)
 			{
-				const float value = frames[i].channel[0] / float(1 << 15);
+				auto & history = histories[c];
 				
-				history.push_back(value);
+				const float value = frames[i].channel[c] / float(1 << 15);
+				
+				history[firstHistoryFrame] = value;
 			}
+			
+			if (numHistoryFrames < maxHistoryFrames)
+				numHistoryFrames++;
+			
+			firstHistoryFrame++;
+			
+			if (firstHistoryFrame == maxHistoryFrames)
+				firstHistoryFrame = 0;
 		}
 	}
 };
 
+static void drawAudioHistory(const MyAudioCallback & audioCallback)
+{
+	gxScalef(CAM_SX / float(audioCallback.maxHistoryFrames - 1), CAM_SY, 1.f);
+
+	for (int c = 0; c < 4; ++c)
+	{
+		auto & history = audioCallback.histories[c];
+		
+		gxPushMatrix();
+		{
+			gxTranslatef(0, (c + 1) / 5.f, 0);
+			gxScalef(1, 1 / 4.f, 1);
+			
+			const Color colors[4] =
+			{
+				colorRed,
+				colorGreen,
+				colorBlue,
+				colorYellow
+			};
+			
+			setColor(colors[c]);
+			hqBegin(HQ_LINES, true);
+			{
+				for (int i = audioCallback.firstHistoryFrame, n = 0; n + 1 < audioCallback.maxHistoryFrames; ++n)
+				{
+					const float value1 = history[i + 0];
+					const float value2 = history[i + 1];
+					const float strokeSize = .4f;
+					
+					hqLine(n + 0, value1, strokeSize, n + 1, value2, strokeSize);
+					
+					//
+					
+					i++;
+					
+					if (i == audioCallback.maxHistoryFrames)
+						i = 0;
+				}
+			}
+			hqEnd();
+		}
+		gxPopMatrix();
+	}
+}
+
 static void testAudioStreaming(PS3EYECam * eye)
 {
-	const int numSeconds = 10;
+	const int testDuration = 10; // seconds
+	const int historySize = 1; // seconds
 	
 	PS3EYEMic mic;
-	MyAudioCallback audioCallback(numSeconds);
+	MyAudioCallback audioCallback(historySize);
 	
 	if (mic.init(eye->getDevice(), &audioCallback))
 	{
-		const auto endTime = SDL_GetTicks() + numSeconds * 1000;
+		const auto endTime = SDL_GetTicks() + testDuration * 1000;
 		
 		while (SDL_GetTicks() < endTime && !keyboard.wentDown(SDLK_SPACE))
 		{
@@ -62,49 +121,7 @@ static void testAudioStreaming(PS3EYECam * eye)
 			
 			framework.beginDraw(0, 0, 0, 0);
 			{
-				auto & baseHistory = audioCallback.histories[0];
-				
-				if (baseHistory.size() < 2)
-					continue;
-				
-				const int numLines = std::min((int)baseHistory.size(), 16000 * 2/3);
-				const int offset = baseHistory.size() - numLines;
-				
-				gxScalef(CAM_SX / float(numLines - 1), CAM_SY, 1.f);
-				
-				for (int c = 0; c < 4; ++c)
-				{
-					auto & history = audioCallback.histories[c];
-					
-					gxPushMatrix();
-					{
-						gxTranslatef(0, (c + 1) / 5.f, 0);
-						gxScalef(1, 1 / 4.f, 1);
-						
-						const Color colors[4] =
-						{
-							colorRed,
-							colorGreen,
-							colorBlue,
-							colorYellow
-						};
-						
-						setColor(colors[c]);
-						hqBegin(HQ_LINES, true);
-						{
-							for (int i = 0; i + 1 < numLines; ++i)
-							{
-								const float value1 = history[i + offset + 0];
-								const float value2 = history[i + offset + 1];
-								const float strokeSize = .4f;
-								
-								hqLine(i + 0, value1, strokeSize, i + 1, value2, strokeSize);
-							}
-						}
-						hqEnd();
-					}
-					gxPopMatrix();
-				}
+				drawAudioHistory(audioCallback);
 			}
 			framework.endDraw();
 		}
@@ -133,7 +150,7 @@ int main(int argc, char * argv[])
 	
 	if (eye != nullptr)
 	{
-		testAudioStreaming(eye.get());
+		//testAudioStreaming(eye.get());
 	}
 	
 	if (eye != nullptr && !eye->init(CAM_SX, CAM_SY, CAM_FPS,
@@ -143,6 +160,14 @@ int main(int argc, char * argv[])
 		errorString = "failed to initialize PS3 eye camera";
 	else
 		eye->start();
+	
+	PS3EYEMic mic;
+	MyAudioCallback audioCallback(.5f);
+	
+	if (eye != nullptr)
+	{
+		mic.init(eye->getDevice(), &audioCallback);
+	}
 	
 	if (!errorString.empty())
 	{
@@ -184,15 +209,19 @@ int main(int argc, char * argv[])
 		
 		framework.beginDraw(0, 0, 0, 0);
 		{
-			setBlend(BLEND_OPAQUE);
-			
 			if (texture != 0)
 			{
-				setColor(colorWhite);
-				gxSetTexture(texture);
-				drawRect(0, 0, CAM_SX, CAM_SY);
-				gxSetTexture(0);
+				pushBlend(BLEND_OPAQUE);
+				{
+					setColor(colorWhite);
+					gxSetTexture(texture);
+					drawRect(0, 0, CAM_SX, CAM_SY);
+					gxSetTexture(0);
+				}
+				popBlend();
 			}
+			
+			drawAudioHistory(audioCallback);
 		}
 		framework.endDraw();
 	}
